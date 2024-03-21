5e3113f52b0d9c6c23c19f9d5665980fafb940d6
FranciscoJose28 <fcojmatos28@gmail.com>
2024-03-21 11:55:35 -0300

    so falta a busca

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
+let lista = [];
+
 function getCategories(){
     fetch('https://fakestoreapi.com/products/categories')
     .then(response => response.json())
     .then(response => {
+        categorias.innerHTML += `<option>Todos</option>`;
         for(let i = 0; i < response.length; i++){
             categorias.innerHTML += `<option>${response[i]}</option>`;
         }
@@ -15,28 +18,50 @@ async function getProducts(){
     try {
         const request = await fetch('https://fakestoreapi.com/products');
         const response = await request.json();
-        produtos.innerHTML = ''
-        if(response.length > 0){
-            response.map(produto => {
-                produtos.innerHTML += `
-                <li class="card">
-                <div class="card-imagem">
-                    <img src="${produto.image}" alt="">
-                    <div class="card-nota">${produto.rating.rate}</div>
-                </div>
-                <div class="card-info">
-                    <h3>${produto.title}</h3>
-                    <h6>${produto.category}</h6>
-                    <h4>$ ${produto.price}</h4>
-                </div>
-            </li>
-                `;
-        })
-        return;
-        }
+        
+        lista = response;
+        renderList(response);
         
     } catch (error) {
         alert("produtos não entrados :/");
     }
 }
 getProducts();
+
+function orderingBy(){
+    if(ordenacao.value == 1){
+        renderList(lista.toSorted((a, b) => a.price - b.price))
+    }else{
+        renderList(lista.toSorted((a, b) => b.rating.rate - a.rating.rate))
+    }
+    // alert("ta funfando" + ordenacao.value)
+}
+
+function filteredBy(){
+    if(categorias.value === 'Todos'){
+        renderList(lista)
+    }else{
+        renderList(lista.filter((item) => item.category == categorias.value))
+    }
+
+    // renderList(lista.toSorted((a, b) => ordenacao.value == 1 ? a.price - b.price))
+}
+
+function renderList(arr){
+    produtos.innerHTML = '';
+    arr.map(produto => {
+        produtos.innerHTML += `
+        <li class="card">
+        <div class="card-imagem">
+            <img src="${produto.image}" alt="">
+            <div class="card-nota">${produto.rating.rate}</div>
+        </div>
+        <div class="card-info">
+            <h3>${produto.title}</h3>
+            <h6>${produto.category}</h6>
+            <h4>$ ${produto.price}</h4>
+        </div>
+    </li>
+        `;
+})
+}
